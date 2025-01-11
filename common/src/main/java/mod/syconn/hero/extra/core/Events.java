398285a282736bef39c744bb227c67ca7e28cf0b
Syconn <49461127+Syconn@users.noreply.github.com>
2025-01-10 19:00:43 -0500

    Port to Architecture API

--- common/src/main/java/mod/syconn/hero/extra/core/Events.java
+++
@@ -1,8 +0,0 @@
-package mod.syconn.hero.extra.core;
-
-import net.minecraft.world.entity.LivingEntity;
-
-public class Events {
-
-    public record LivingEntityFallEvent(LivingEntity entity, float distance, float damageMultiplier, boolean cancel) {}
-}
